fe841ca14d2b1b035a11a88252cc2c3595f4223f
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-09-27 19:55:34 -0400

    completed preprocessing, added dataset builder

--- hw2/src/preprocessing.py
+++ hw2/src/preprocessing.py
@@ -2,29 +2,97 @@ import numpy as np
 import spacy
 import glob
 
-feature_dict = {}
-feature_counts = {}
+def build_dataset():
 
-# spaCy english language model
-nlp = spacy.load("en_core_web_sm")
+    feature_map = {}
 
-# read autos docs, ignoring header portions of files
-for file in glob.glob("../train/rec.autos/*"):
-    with open(file) as f:
-        all_lines = f.readlines()
-        i = 0
-        while "Lines:" not in all_lines[i]:
+    # spaCy english language model
+    nlp = spacy.load("en_core_web_sm")
+
+    # num_autos = 0
+    # num_hockey = 0
+
+    X = []
+    y = []
+
+    # read autos docs, ignoring header portions of files
+    index = 0
+    for file in glob.glob("../train/rec.autos/*"):
+
+        with open(file) as f:
+            all_lines = f.readlines()
+            i = 0
+            while "Lines:" not in all_lines[i]:
+                i += 1
+
+            num_lines = int(all_lines[i].split(":")[1])
             i += 1
 
-        num_lines = int(all_lines[i].split(":")[1])
-        i += 1
+            rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
+            
+            # extract parts of speech, stop word status, etc. for each token
+            doc_tokens = nlp(rel_string)
 
-        rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
-        
-        # extract parts of speech, stop word status, etc. for each token
-        doc_tokens = nlp(rel_string)
+            # track word frequencies within the current document
+            # feature_counts = {}
+            curr_doc = [0] * len(feature_map)
 
-        for token in doc_tokens:
-            if token.pos_ != "X" and not token.is_stop:
-                to_lower = token.text.lower()
-                feature_counts[to_lower] = feature_dict.setdefault(to_lower, 0) + 1
+            for token in doc_tokens:
+                if token.pos_ != 'X' and not token.is_stop:
+                    token_lower = token.text.lower()
+                    # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
+                    if token.text not in feature_map:
+                        feature_map[token.text] = index
+                        index += 1
+                        curr_doc.append(1)
+                    else:
+                        curr_doc[feature_map[token.text]] += 1
+            
+            X.append(curr_doc)
+            y.append(0)
+
+
+    # read hockey docs, ignoring headers
+    for file in glob.glob("../train/rec.sport.hockey/*"):
+
+        with open(file) as f:
+            all_lines = f.readlines()
+            i = 0
+            while "Lines:" not in all_lines[i]:
+                i += 1
+
+            num_lines = int(all_lines[i].split(":")[1])
+            i += 1
+
+            rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
+            
+            # extract parts of speech, stop word status, etc. for each token
+            doc_tokens = nlp(rel_string)
+
+            # track word frequencies within the current document
+            # feature_counts = {}
+            curr_doc = [0] * len(feature_map)
+
+            for token in doc_tokens:
+                if token.pos_ != 'X' and not token.is_stop:
+                    token_lower = token.text.lower()
+                    # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
+                    if token.text not in feature_map:
+                        feature_map[token.text] = index
+                        index += 1
+                        curr_doc.append(1)
+                    else:
+                        curr_doc[feature_map[token.text]] += 1
+            
+            X.append(curr_doc)
+            y.append(1)
+
+    # equalize the feature set lengths for all training examples
+    for doc in X[:-1]:
+        if len(doc) < len(X[-1]):
+            doc.extend([0] * (len(X[-1]) - len(doc)))
+
+    X = np.asarray(X)
+    y = np.asarray(y)
+
+    return X, y, feature_map
