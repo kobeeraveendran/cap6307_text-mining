dec2b7cb3fb2ecc78b55d24e4708960602b75843
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-09-27 22:07:26 -0400

    fixed training set preparation

--- hw2/src/preprocessing.py
+++ hw2/src/preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import spacy
 import glob
 
+
 def build_dataset():
 
     feature_map = {}
@@ -18,15 +19,19 @@ def build_dataset():
     # read autos docs, ignoring header portions of files
     index = 0
     for file in glob.glob("../train/rec.autos/*"):
-
-        with open(file) as f:
+        #print(file)
+        with open(file, encoding = 'utf8', errors = 'ignore') as f:
             all_lines = f.readlines()
             i = 0
-            while "Lines:" not in all_lines[i]:
+            while i < len(all_lines) and "Lines:" not in all_lines[i]:
                 i += 1
 
-            num_lines = int(all_lines[i].split(":")[1])
-            i += 1
+            if i >= len(all_lines):
+                i = 5
+                num_lines = len(all_lines) - i
+            else:
+                num_lines = int(all_lines[i].split(":")[1])
+                i += 1
 
             rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
             
@@ -38,7 +43,7 @@ def build_dataset():
             curr_doc = [0] * len(feature_map)
 
             for token in doc_tokens:
-                if token.pos_ != 'X' and not token.is_stop:
+                if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
                     token_lower = token.text.lower()
                     # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
                     if token.text not in feature_map:
@@ -51,18 +56,25 @@ def build_dataset():
             X.append(curr_doc)
             y.append(0)
 
+    print("Processed autos training set!")
+
 
     # read hockey docs, ignoring headers
     for file in glob.glob("../train/rec.sport.hockey/*"):
 
-        with open(file) as f:
+        with open(file, encoding = 'utf8', errors = 'ignore') as f:
+
             all_lines = f.readlines()
             i = 0
-            while "Lines:" not in all_lines[i]:
+            while i < len(all_lines) and "Lines:" not in all_lines[i]:
                 i += 1
-
-            num_lines = int(all_lines[i].split(":")[1])
-            i += 1
+            
+            if i >= len(all_lines):
+                i = 5
+                num_lines = len(all_lines) - i
+            else:
+                num_lines = int(all_lines[i].split(":")[1])
+                i += 1
 
             rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
             
@@ -74,7 +86,7 @@ def build_dataset():
             curr_doc = [0] * len(feature_map)
 
             for token in doc_tokens:
-                if token.pos_ != 'X' and not token.is_stop:
+                if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
                     token_lower = token.text.lower()
                     # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
                     if token.text not in feature_map:
@@ -87,6 +99,8 @@ def build_dataset():
             X.append(curr_doc)
             y.append(1)
 
+    print("Processed hockey training set!")
+
     # equalize the feature set lengths for all training examples
     for doc in X[:-1]:
         if len(doc) < len(X[-1]):
@@ -96,3 +110,8 @@ def build_dataset():
     y = np.asarray(y)
 
     return X, y, feature_map
+
+if __name__ == "__main__":
+
+    X, y, feature_map = build_dataset()
+    
