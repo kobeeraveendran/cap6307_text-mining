6342a32d1f801d676ca1d7cdeb80143f3fb18789
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-09-29 18:53:37 -0400

    added timers, display output

--- hw2/src/text_classifier.py
+++ hw2/src/text_classifier.py
@@ -2,6 +2,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import f1_score
 import numpy as np
 from preprocessing import build_dataset
+import time
 
 if __name__ == "__main__":
 
@@ -9,8 +10,24 @@ if __name__ == "__main__":
     clf = MultinomialNB(alpha = 1.0, fit_prior = True)
     
     # load dataset after preprocessing
+
+    print("Loading dataset...")
+    start_load = time.time()
     X_train, y_train, X_test, y_test, feature_map = build_dataset()
+    end = time.time()
+    print("\nDataset loaded. Total time elapsed: {:.2f}s".format(end - start_load))
+
+    print("\nNumber of training examples: ", X_train.shape[0])
+    print("Number of test examples: ", X_test.shape[0])
+    print("Number of unique words in feature map: ", X_train.shape[1])
+
+    print("\nFitting Multinomial Naive Bayes model...")
+
+    start = time.time()
     clf.fit(X_train, y_train)
+    end = time.time()
+    
+    print("\nTraining time elapsed: {:.2f}s".format(end - start))
 
     preds = clf.predict(X_test)
     pos_score = f1_score(y_test, preds, pos_label = 1, average = 'binary')
@@ -18,9 +35,9 @@ if __name__ == "__main__":
     acc = clf.score(X_test, y_test)
 
     #pred_acc = clf.score(X_test, y_test)
-    print("F1 score for positive class: ", pos_score)
-    print("F1 score for negative class: ", neg_score)
-    print("Accuracy: ", acc)
+    print("\nF1 score for positive class (hockey): ", pos_score)
+    print("F1 score for negative class (autos): ", neg_score)
+    print("Mean Accuracy: {:.2f}%".format(acc * 100))
 
-    print("\nPredictions:\n", preds)
-    print("\nGround truth:\n", y_test)
+    end = time.time()
+    print("\nTotal time elapsed: {:.2f}s".format(end - start_load))
