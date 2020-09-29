a56175ad5a762cb28bfcc6b9c51f69f9c2b5a5bc
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-09-29 17:04:52 -0400

    fixed bug where text added to feature map was not lowercased, now loads in correct datasets for test set

--- hw2/src/preprocessing.py
+++ hw2/src/preprocessing.py
@@ -13,12 +13,12 @@ def build_dataset():
     # num_autos = 0
     # num_hockey = 0
 
-    X = []
-    y = []
+    X_train = []
+    y_train = []
 
     # read autos docs, ignoring header portions of files
     index = 0
-    for file in glob.glob("../train/rec.autos/*"):
+    for filenum, file in enumerate(glob.glob("../train/rec.autos/*")):
         #print(file)
         with open(file, encoding = 'utf8', errors = 'ignore') as f:
             all_lines = f.readlines()
@@ -46,18 +46,20 @@ def build_dataset():
                 if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
                     token_lower = token.text.lower()
                     # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
-                    if token.text not in feature_map:
-                        feature_map[token.text] = index
+                    if token_lower not in feature_map:
+                        feature_map[token_lower] = index
                         index += 1
                         curr_doc.append(1)
                     else:
-                        curr_doc[feature_map[token.text]] += 1
+                        curr_doc[feature_map[token_lower]] += 1
+                    
+                    # if filenum == 0 :
+                    #     print("Valid token: ", token_lower, "  Index = ", feature_map[token_lower])
             
-            X.append(curr_doc)
-            y.append(0)
+            X_train.append(curr_doc)
+            y_train.append(0)
 
     print("Processed autos training set!")
-
 
     # read hockey docs, ignoring headers
     for file in glob.glob("../train/rec.sport.hockey/*"):
@@ -89,34 +91,118 @@ def build_dataset():
                 if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
                     token_lower = token.text.lower()
                     # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
-                    if token.text not in feature_map:
-                        feature_map[token.text] = index
+                    if token_lower not in feature_map:
+                        feature_map[token_lower] = index
                         index += 1
                         curr_doc.append(1)
                     else:
-                        curr_doc[feature_map[token.text]] += 1
+                        curr_doc[feature_map[token_lower]] += 1
             
-            X.append(curr_doc)
-            y.append(1)
+            X_train.append(curr_doc)
+            y_train.append(1)
 
     print("Processed hockey training set!")
 
     # equalize the feature set lengths for all training examples
-    for doc in X[:-1]:
-        if len(doc) < len(X[-1]):
-            doc.extend([0] * (len(X[-1]) - len(doc)))
+    for doc in X_train[:-1]:
+        if len(doc) < len(X_train[-1]):
+            doc.extend([0] * (len(X_train[-1]) - len(doc)))
 
-    X_train = np.asarray(X)
-    y_train = np.asarray(y)
+    X_train = np.asarray(X_train)
+    y_train = np.asarray(y_train)
 
     X_train, y_train = shuffle(X_train, y_train)
 
+    X_test = []
+    y_test = []
 
-    return X_train, y_train, feature_map
+    # parse and load in test set
+    for file in glob.glob("../test/rec.autos/*"):
+
+        with open(file, encoding = 'utf8', errors = 'ignore') as f:
+
+            all_lines = f.readlines()
+            i = 0
+            while i < len(all_lines) and "Lines:" not in all_lines[i]:
+                i += 1
+            
+            if i >= len(all_lines):
+                i = 5
+                num_lines = len(all_lines) - i
+            else:
+                num_lines = int(all_lines[i].split(":")[1])
+                i += 1
+
+            rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
+            
+            # extract parts of speech, stop word status, etc. for each token
+            doc_tokens = nlp(rel_string)
+
+            # track word frequencies within the current document
+            # feature_counts = {}
+            curr_doc = [0] * len(feature_map)
+
+            for token in doc_tokens:
+                if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
+                    
+                    token_lower = token.text.lower()
+                    if token_lower in feature_map:
+                        curr_doc[feature_map[token_lower]] += 1
+            
+            X_test.append(curr_doc)
+            y_test.append(0)
+
+    print("Processed autos test set!")
+    
+    for file in glob.glob("../test/rec.sport.hockey/*"):
+
+        with open(file, encoding = 'utf8', errors = 'ignore') as f:
+
+            all_lines = f.readlines()
+            i = 0
+            while i < len(all_lines) and "Lines:" not in all_lines[i]:
+                i += 1
+            
+            if i >= len(all_lines):
+                i = 5
+                num_lines = len(all_lines) - i
+            else:
+                num_lines = int(all_lines[i].split(":")[1])
+                i += 1
+
+            rel_string = ''.join([line[:-1] for line in all_lines[i:i + num_lines]])
+            
+            # extract parts of speech, stop word status, etc. for each token
+            doc_tokens = nlp(rel_string)
+
+            # track word frequencies within the current document
+            # feature_counts = {}
+            curr_doc = [0] * len(feature_map)
+
+            for token in doc_tokens:
+                if token.pos_ != 'X' and token.text.isalpha() and not token.is_stop:
+                    token_lower = token.text.lower()
+                    # feature_counts[token_lower] = feature_counts.setdefault(token_lower, 0) + 1
+                    if token_lower in feature_map:
+                        curr_doc[feature_map[token_lower]] += 1
+            
+            X_test.append(curr_doc)
+            y_test.append(1)
+
+    print("Processed hockey test set!")
+
+    X_test = np.asarray(X_test)
+    y_test = np.asarray(y_test)
+
+    X_test, y_test = shuffle(X_test, y_test)
+
+    return X_train, y_train, X_test, y_test, feature_map
 
 if __name__ == "__main__":
 
-    X, y, feature_map = build_dataset()
-    print("X shape: ", X.shape)
-    print("y shape: ", y.shape)
+    X_train, y_train, X_test, y_test, feature_map = build_dataset()
+    print("X_train shape: ", X_train.shape)
+    print("y_train shape: ", y_train.shape)
+    print("X_test shape: ", X_test.shape)
+    print("y_test shape: ", y_test.shape)
     print("Feature set size: ", len(feature_map))
