2044a3be0f6293680a507bcce3fbaef1bbcac28d
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-11-11 18:30:17 -0500

    save results to json; clean up + document code

--- HW3/src/rouge_summary_eval.py
+++ HW3/src/rouge_summary_eval.py
@@ -1,16 +1,14 @@
 from pyrouge import Rouge155
 
 import time
-
 import glob
+import json
 
 rouge = Rouge155()
 
-# rouge.model_dir = "../Human_Summaries/eval/"
-# rouge.model_filename_pattern = "D(\d+).M.100.T.[A-Za-z]+"
+output_dicts = {}
 
-output_dicts = []
-
+# generates ROUGE scores for each system's summaries, directory by directory
 for system in glob.glob("../System_Summaries/*/"):
     rouge.system_dir = system
     rouge.system_filename_pattern = "d(\d+)t.[A-Za-z]+"
@@ -24,11 +22,16 @@ for system in glob.glob("../System_Summaries/*/"):
 
     end = time.time()
 
+    # show avg. results across each metric, time elapsed, etc.
     print("RESULTS FOR {}".format(system))
-    print("TIME ELAPSED: {:.2f}".format(end - start))
+    print("TIME ELAPSED: {:.2f}s".format(end - start))
     print(output)
-    output_dict = rouge.output_to_dict(output)
-    output_dicts.append(output_dict)
 
-# print("OUTPUT_DICTS")
-# print(output_dicts)
+    # save each system's scores to a dictionary
+    output_dict = rouge.output_to_dict(output)
+    system_name = system.split('/')[-2]
+    output_dicts[system_name] = output_dict
+
+# save the output dictionaries as a json file and prettify it for readabillity
+with open("../rouge_scores.json", "w") as output_file:
+    json.dump(output_dicts, output_file, indent = 4)
